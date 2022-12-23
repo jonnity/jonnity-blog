79c71291e200ee49cd8dae0876f9ec097b3961c2
jonnity <jonny1996_ty@yahoo.co.jp>
2022-12-23 23:38:19 +0900

    SSGを試せるように設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import { GetStaticProps, NextPage } from "next";
+import { useEffect, useState } from "react";
 import Title from "../components/Title";
 
-export default () => {
-  return <Title title="test title"></Title>;
+type Props = { res: string };
+export const getStaticProps: GetStaticProps<Props> = async (_ctx) => {
+  const res = await fetch("https://httpbin.org/get");
+  const text = await res.text();
+  return {
+    props: { res: text }, // will be passed to the page component as props
+  };
 };
+const TestPage: NextPage<Props> = ({ res }) => {
+  const [text, setText] = useState<string>("");
+  useEffect(() => {
+    setTimeout(() => setText("hogehoge"), 1000);
+  });
+
+  return <Title title={text ? text : res}></Title>;
+};
+
+export default TestPage;
